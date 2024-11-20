f078576beff3491d7a9f506814e6dce7fd73c09d
Gauravjot Garaya <gauravjot@outlook.com>
2024-11-20 07:57:02 -0800

    fixed middleware imports in settings.py

--- django_axor_auth/extras/middlewares.py
+++ django_axor_auth/extras/middlewares.py
@@ -85,26 +85,3 @@ class VerifyRequestOriginMiddleware:
         if request.headers.get('Origin') in settings.ALLOW_ORIGINS:
             return True
         return False
-
-
-class HeaderRequestedByMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        # Run on API requests only
-        if request.path.startswith(config.URI_PREFIX):
-            # If 'X-Requested-By' header is present and not set to 'web'
-            # then token based authentication is used. In such case,
-            # provide 'Authorization' header with token.
-            if 'HTTP_X_REQUESTED_BY' in request.META:
-                request.requested_by = request.META['HTTP_X_REQUESTED_BY']
-            else:
-                request.requested_by = 'web'
-
-        response = self.get_response(request)
-        return response
-
-
-def is_web(request):
-    return request.requested_by == 'web'
