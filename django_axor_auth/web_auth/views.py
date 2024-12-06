af6c29cc48e82851c5b350cbf18b27dd34f366c2
Gauravjot Garaya <gauravjot@outlook.com>
2024-12-05 22:05:44 -0800

    added magic link request support

--- django_axor_auth/web_auth/views.py
+++ django_axor_auth/web_auth/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.utils.encoding import force_str
 from .forms import LoginForm
 from django_axor_auth.users.views import login, me
+from django_axor_auth.users.users_magic_link.views import request_magic_link
 import json
 from django_axor_auth.configurator import config
 
@@ -18,7 +20,9 @@ def login_page(request):
     # Check if there is a login request
     if request.method == "POST":
         form = LoginForm(request.POST)
-        if form.is_valid():
+        # Check if it was passwordless login
+        is_passwordless = request.GET.get('method') == 'passwordless'
+        if not is_passwordless and form.is_valid():
             request.data = form.cleaned_data
             api_res = login(request)
             if api_res.status_code >= 400:
@@ -34,6 +38,13 @@ def login_page(request):
                 response = render(request, template, {'app': app_info, 'success': True})
                 response.cookies = api_res.cookies
                 return response
+        elif is_passwordless:
+            # Passwordless login
+            email = force_str(form.data.get('email'))
+            request.data = {'email': email}
+            api_res = request_magic_link(request)
+            return render(request, template, {'app': app_info, 'success': True, 'passwordless': True})
+        return render(request, template, {'app': app_info, 'error': 'Please enter email and password.', 'form': form, 'passwordless': is_passwordless})
     else:
         # Check if user is already logged in
         user = me(request)
@@ -43,6 +54,10 @@ def login_page(request):
         else:
             # User is not logged in
             form = LoginForm()
+            # Check if ?method=passwordless is in the URL
+            if request.GET.get('method') == 'passwordless':
+                return render(request, template, {'app': app_info, 'passwordless': True, 'form': form})
+            # default
             return render(request, template, {'app': app_info, 'form': form})
 
 
